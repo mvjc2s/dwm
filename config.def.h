bf4dc11772ad9e9492b0a4830886e84dba161e27
Marcus Vinicius Salvador dos Santos <marcus.v.salvador92@protonmail.com>
2025-10-05 15:46:27 -0300

    Patch 14 - rotatestack: this feature enables you rotate your stack in a smooth style

--- config.def.h
+++ config.def.h
@@ -120,6 +120,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_k,			   focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,			   incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,			   incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      		   rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,              rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_h,			   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,			   setmfact,       {.f = +0.05} },
     { MODKEY|ShiftMask,             XK_Return,		   zoom,           {0} },
