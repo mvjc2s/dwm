fa896707aea70df4013cced3d9ab82bf0d1ac6f7
Marcus Vinicius Salvador dos Santos <marcus.v.salvador92@protonmail.com>
2025-10-07 20:36:14 -0300

    config.def.h: I'm adding reasonable constants

--- config.def.h
+++ config.def.h
@@ -1,5 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
+/* Constants */
+#define TERMINAL "st"
+#define TERMCLASS "st-256color"
+#define browser "firefox"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -18,7 +23,7 @@ static const char *fonts[]          = {
 		"Noto Sans:size=11",
 		"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "Iosevka Nerd Font:size=10";
+static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -54,7 +59,7 @@ static const Rule rules[] = {
 	{ "Gimp",    			NULL,     NULL,           0,         1,			    -1,		-1,		-1,		-1,         	0,          0,        -1 },
 	{"Qalculate-gtk",		NULL,	  NULL,			  0,		 1,				.35,	35,		.3,		.5,				0,			0,		  -1 },
 	{ "Firefox", 			NULL,     NULL,           1 << 8,    0,				-1,		-1,		-1,		-1, 			0,         -1,        -1 },
-	{ "st-256color",      	NULL,     NULL,           0,         0,				-1,		-1,		-1,		-1,				1,          0,        -1 },
+	{ TERMCLASS,      	NULL,     NULL,           0,         0,				-1,		-1,		-1,		-1,				1,          0,        -1 },
 	{ NULL,      			NULL,     "Event Tester", 0,         0,				-1,		-1,		-1,		-1,				0,          1,        -1 }, /* xev */
 };
 
@@ -113,7 +118,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *layoutmenu_cmd = "layoutmenu.sh";
 static const scratchpad qalculate = {.class = "Qalculate-gtk", .v = (char *[]){"qalculate-gtk", NULL}};
 
