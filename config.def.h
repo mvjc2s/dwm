a6d34025e320b92be0c99843a2509dc6f11130e4
Marcus Vinicius Salvador dos Santos <marcus.v.salvador92@protonmail.com>
2025-10-31 20:10:32 -0300

    patch 26: statuspadding

--- config.def.h
+++ config.def.h
@@ -36,6 +36,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 0;        /* vertical padding for statusbar */
 static const char *fonts[]          = {
 		"FiraCode Nerd Font:size=12",
     "JetBrains Mono:size=12:antialias=true:autohint=true",
