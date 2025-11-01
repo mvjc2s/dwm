fe4d66b7c7493eb5e00b79e0cb39e38d41fdf0a4
Marcus Vinicius Salvador dos Santos <marcus.v.salvador92@protonmail.com>
2025-10-31 22:41:45 -0300

    config.def.h: restructuring and reasonable keybindings modification

--- config.def.h
+++ config.def.h
@@ -49,37 +49,32 @@ static const int refreshrate            = 120;      /* refresh rate (per second)
 static       int attachbelow            = 1;        /* 1 means attach after the currently active window */
 static       int smartgaps              = 0;        /* 1 means no outer gap when there is only one window */
 static const float mfact                = 0.55;     /* factor of master area size [0.05..0.95] */
-static const char *fonts[]              = {
-	  "FiraCode Nerd Font:size=12",
-    "JetBrains Mono:size=12:antialias=true:autohint=true",
-	  "Iosevka Nerd Font:size=12",
-	  "Hack Nerd Font:size=12",
-	  "Noto Sans:size=12",
-	  "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
-};
-static const char dmenufont[]       = "FiraCode Nerd Font:size=12:antialias=true:autohint=true";
+static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
+static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
+
+// Colors variables
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
-static char termcol0[] = "#000000"; /* black   */
-static char termcol1[] = "#ff0000"; /* red     */
-static char termcol2[] = "#33ff00"; /* green   */
-static char termcol3[] = "#ff0099"; /* yellow  */
-static char termcol4[] = "#0066ff"; /* blue    */
-static char termcol5[] = "#cc00ff"; /* magenta */
-static char termcol6[] = "#00ffff"; /* cyan    */
-static char termcol7[] = "#d0d0d0"; /* white   */
-static char termcol8[]  = "#808080"; /* black   */
-static char termcol9[]  = "#ff0000"; /* red     */
-static char termcol10[] = "#33ff00"; /* green   */
-static char termcol11[] = "#ff0099"; /* yellow  */
-static char termcol12[] = "#0066ff"; /* blue    */
-static char termcol13[] = "#cc00ff"; /* magenta */
-static char termcol14[] = "#00ffff"; /* cyan    */
-static char termcol15[] = "#ffffff"; /* white   */
+static char termcol0[]              = "#000000"; /* black   */
+static char termcol1[]              = "#ff0000"; /* red     */
+static char termcol2[]              = "#33ff00"; /* green   */
+static char termcol3[]              = "#ff0099"; /* yellow  */
+static char termcol4[]              = "#0066ff"; /* blue    */
+static char termcol5[]              = "#cc00ff"; /* magenta */
+static char termcol6[]              = "#00ffff"; /* cyan    */
+static char termcol7[]              = "#d0d0d0"; /* white   */
+static char termcol8[]              = "#808080"; /* black   */
+static char termcol9[]              = "#ff0000"; /* red     */
+static char termcol10[]             = "#33ff00"; /* green   */
+static char termcol11[]             = "#ff0099"; /* yellow  */
+static char termcol12[]             = "#0066ff"; /* blue    */
+static char termcol13[]             = "#cc00ff"; /* magenta */
+static char termcol14[]             = "#00ffff"; /* cyan    */
+static char termcol15[]             = "#ffffff"; /* white   */
 static char *termcolor[] = {
   termcol0,
   termcol1,
@@ -102,16 +97,26 @@ static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+}; 
+
+
+// Font variables
+static const char *fonts[]              = {
+	  "FiraCode Nerd Font:size=12",
+    "JetBrains Mono:size=12:antialias=true:autohint=true",
+	  "Iosevka Nerd Font:size=12",
+	  "Hack Nerd Font:size=12",
+	  "Noto Sans:size=12",
+	  "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
 };
-static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
-static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
-
-// Vanity gaps definitions
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#define ICONSIZE 16   /* icon size */
-#define ICONSPACING 5 /* space between icon and title */
-#include "vanitygaps.c"
-
+static const char dmenufont[]       = "FiraCode Nerd Font:size=12:antialias=true:autohint=true";
+ 
+ // Vanity gaps definitions
+ #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+ #define ICONSIZE 16   /* icon size */
+ #define ICONSPACING 5 /* space between icon and title */
+ #include "vanitygaps.c"
+ 
 // Tagging
 static const char *tags[] = {
   "1:🔍", /* information gathering / identification */
@@ -203,7 +208,7 @@ static const Keychord *keychords[] = {
   &((Keychord){2, {{MODKEY, XK_c}, {ShiftMask, XK_BackSpace}},                   killclient,         {.ui = 1 } }), // kill all unselected windows
   &((Keychord){2, {{MODKEY, XK_c}, {ControlMask|ShiftMask, XK_BackSpace}},       killclient,         {.ui = 2 } }), // kill all windows
   &((Keychord){2, {{MODKEY, XK_c}, {0, XK_q}},                                   quit,               {0} }),        // quit dwm
-  &((Keychord){2, {{MODKEY, XK_c}, {ShiftMask, XK_q}},                           quit,               {1} }),        // restart dwm
+  &((Keychord){2, {{MODKEY, XK_c}, {0, XK_r}},                                   quit,               {1} }),        // restart dwm
   &((Keychord){2, {{MODKEY, XK_c}, {0, XK_b}},                                   togglebar,          {0} }),
   &((Keychord){2, {{MODKEY, XK_c}, {0, XK_s}},                                   togglesticky,       {0} }),
   &((Keychord){2, {{MODKEY, XK_c}, {0, XK_a}},                                   toggleAttachBelow,  {0} }),
@@ -224,44 +229,45 @@ static const Keychord *keychords[] = {
   &((Keychord){1, {{MODKEY, XK_d}},                                              incnmaster,         {.i = -1 } }),
   &((Keychord){1, {{MODKEY, XK_h}},                                              setmfact,           {.f = -0.05} }),
   &((Keychord){1, {{MODKEY, XK_l}},                                              setmfact,           {.f = +0.05} }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_f}},                                   focusmaster,        {0} }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_u}},                                   incrgaps,           {.i = +1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {ShiftMask, XK_u}},                           incrgaps,           {.i = -1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_i}},                                   incrigaps,          {.i = +1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {ShiftMask, XK_i}},                           incrigaps,          {.i = -1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_o}},                                   incrogaps,          {.i = +1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {ShiftMask, XK_o}},                           incrogaps,          {.i = -1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {0, XK_i}},                         incrihgaps,         {.i = +1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {ShiftMask, XK_i}},                 incrihgaps,         {.i = -1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {0, XK_v}},                         incrivgaps,         {.i = +1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {ShiftMask, XK_v}},                 incrivgaps,         {.i = -1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {0, XK_h}},                         incrohgaps,         {.i = +1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {ShiftMask, XK_h}},                 incrohgaps,         {.i = -1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {0, XK_o}},                         incrovgaps,         {.i = +1 } }),
-  &((Keychord){2, {{MODKEY|ShiftMask, XK_s}, {ShiftMask, XK_o}},                 incrovgaps,         {.i = -1 } }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_g}},                                   togglegaps,         {0} }),
-  &((Keychord){2, {{MODKEY, XK_s}, {0, XK_d}},                                   defaultgaps,        {0} }),
+  &((Keychord){2, {{MODKEY, XK_f}, {0, XK_m}},                                   focusmaster,        {0} }),
+  &((Keychord){2, {{MODKEY, XK_g}, {0, XK_t}},                                   togglegaps,         {0} }),
+  &((Keychord){2, {{MODKEY, XK_g}, {0, XK_d}},                                   defaultgaps,        {0} }),
+  &((Keychord){2, {{MODKEY, XK_g}, {0, XK_e}},                                   incrgaps,           {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_g}, {ShiftMask, XK_e}},                           incrgaps,           {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_g}, {0, XK_i}},                                   incrigaps,          {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_g}, {ShiftMask, XK_i}},                           incrigaps,          {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_g}, {0, XK_o}},                                   incrogaps,          {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_g}, {ShiftMask, XK_o}},                           incrogaps,          {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_h}, {0, XK_i}},                                   incrihgaps,         {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_h}, {ShiftMask, XK_i}},                           incrihgaps,         {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_h}, {0, XK_o}},                                   incrohgaps,         {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_h}, {ShiftMask, XK_o}},                           incrohgaps,         {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_v}, {0, XK_i}},                                   incrivgaps,         {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_v}, {ShiftMask, XK_i}},                           incrivgaps,         {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_v}, {0, XK_o}},                                   incrovgaps,         {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_v}, {ShiftMask, XK_o}},                           incrovgaps,         {.i = -1 } }),
 
-  // Layouts keybinds
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_t}},                                   setlayout,          {.v = &layouts[0] } }),      // layout[0]: tile
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_f}},                                   setlayout,          {.v = &layouts[1] } }),      // layout[1]: no layout function = floating behavior
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_m}},                                   setlayout,          {.v = &layouts[2] } }),      // layout[2]: monocle
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_b}},                                   setlayout,          {.v = &layouts[3] } }),      // layout[3]: bottom stack
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_b}},                                   setlayout,          {.v = &layouts[4] } }),      // layout[4]: bottom stack horizontal
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_s}},                                   setlayout,          {.v = &layouts[5] } }),      // layout[5]: spiral
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_d}},                                   setlayout,          {.v = &layouts[6] } }),      // layout[6]: dwindle
-  &((Keychord){2, {{MODKEY, XK_l}, {ShiftMask, XK_d}},                           setlayout,          {.v = &layouts[7] } }),      // layout[7]: deck
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_g}},                                   setlayout,          {.v = &layouts[8] } }),      // layout[8]: grid
-  &((Keychord){2, {{MODKEY, XK_l}, {ShiftMask, XK_g}},                           setlayout,          {.v = &layouts[9] } }),      // layout[9]: n + 1 row grid
-  &((Keychord){2, {{MODKEY, XK_l}, {ControlMask, XK_g}},                         setlayout,          {.v = &layouts[10] } }),     // layout[10]: gapless grid
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_h}},                                   setlayout,          {.v = &layouts[11] } }),     // layout[11]: horizontal grid
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_c}},                                   setlayout,          {.v = &layouts[12] } }),     // layout[12]: centered master
-  &((Keychord){2, {{MODKEY, XK_l}, {ShiftMask, XK_c}},                           setlayout,          {.v = &layouts[13] } }),     // layout[13]: centered floating master
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_space}},                               setlayout,          {0} }),                      // back to tile layout
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_period}},                              cyclelayout,        {.i = +1 } }),
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_comma}},                               cyclelayout,        {.i = -1 } }),
-  &((Keychord){2, {{MODKEY, XK_l}, {0, XK_space}},                               togglefloating,     {0} }),
-  &((Keychord){2, {{MODKEY, XK_l}, {ShiftMask, XK_m}},                           layoutmenu,         {0} }),
+
+  // Windows layouts keybindings
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_t}},                         setlayout,          {.v = &layouts[0] } }),      // layout[0]: tile
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_f}},                         setlayout,          {.v = &layouts[1] } }),      // layout[1]: no layout function = floating behavior
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_m}},                         setlayout,          {.v = &layouts[2] } }),      // layout[2]: monocle
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_b}},                         setlayout,          {.v = &layouts[3] } }),      // layout[3]: bottom stack
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_b}},                         setlayout,          {.v = &layouts[4] } }),      // layout[4]: bottom stack horizontal
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_s}},                         setlayout,          {.v = &layouts[5] } }),      // layout[5]: spiral
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_d}},                         setlayout,          {.v = &layouts[6] } }),      // layout[6]: dwindle
+  &((Keychord){2, {{MODKEY, XK_w}, {ShiftMask, XK_d}},                 setlayout,          {.v = &layouts[7] } }),      // layout[7]: deck
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_g}},                         setlayout,          {.v = &layouts[8] } }),      // layout[8]: grid
+  &((Keychord){2, {{MODKEY, XK_w}, {ShiftMask, XK_g}},                 setlayout,          {.v = &layouts[9] } }),      // layout[9]: n + 1 row grid
+  &((Keychord){2, {{MODKEY, XK_w}, {ControlMask, XK_g}},               setlayout,          {.v = &layouts[10] } }),     // layout[10]: gapless grid
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_h}},                         setlayout,          {.v = &layouts[11] } }),     // layout[11]: horizontal grid
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_c}},                         setlayout,          {.v = &layouts[12] } }),     // layout[12]: centered master
+  &((Keychord){2, {{MODKEY, XK_w}, {ShiftMask, XK_c}},                 setlayout,          {.v = &layouts[13] } }),     // layout[13]: centered floating master
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_space}},                     setlayout,          {0} }),                      // back to tile layout
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_period}},                    cyclelayout,        {.i = +1 } }),
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_comma}},                     cyclelayout,        {.i = -1 } }),
+  &((Keychord){2, {{MODKEY, XK_w}, {0, XK_space}},                     togglefloating,     {0} }),
+  &((Keychord){2, {{MODKEY, XK_w}, {ShiftMask, XK_m}},                 layoutmenu,         {0} }),
 
 
   // X11 keybindings
